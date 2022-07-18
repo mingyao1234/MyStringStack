b530e3c009205203f951899b8c313faaf4e9aed4
mingyao1234 <21023135@myrp.edu.sg>
2022-07-18 10:40:21 +0800

    first push

---
+++ MyStringStack/src/MyStringStack.java
@@ -0,0 +1,26 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Quah Ming Yao
+ * Student ID: 21023135
+ * Class: W64N
+ * Date/Time created: Sunday 17-07-2022 22:51
+ */
+
+/**
+ * @author MY_NB
+ *
+ */
+public class MyStringStack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Hello World");
+	}
+
+}
